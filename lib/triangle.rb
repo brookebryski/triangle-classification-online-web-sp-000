afd4a5d96a6e709a5792956b9b92c096ef3ff1ce
chic-state-0895 <brookeabryski@gmail.com>
2020-09-17 21:43:30 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,33 @@
 class Triangle
-  # write code here
+
+  attr_accessor :a, :b, :c
+
+  def initialize(a,b,c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+def triangle?
+  if ((a + b > c) || (b + c > a) || (a + c > b)) && ((a > 0) && (b > 0) && (c > 0))
+    return true
+  if Triangle.new(0, 0, 0)
+     raise TriangleError
+   else raise TriangleError
+end
+end
+
+  def kind
+  if ((a == b) && (a == c) && (b == c))
+    return :equilateral
+  elsif ((a == b) || (a == c) || (b == c))
+    return :isosceles
+  else
+    return :scalene
+  end
+end
+
+
+  class TriangleError < StandardError
+  end
 end
